486c104333714a0ff8ae794393741d6a76fd1423
Adam Zabloudil <adampzab@gmail.com>
2023-04-28 13:12:57 -0700

    Initial Commit

---
+++ vpnApp/vpnAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  vpnAppApp.swift
+//  vpnApp
+//
+//  Created by Adam Zabloudil on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct vpnAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
